013aff31527e8403d734ce61386ccd59a67c580a
Busyaska <fox666g@gmail.com>
2025-04-17 21:23:26 +0500

    create lab4

---
+++ lab4/download.py
@@ -0,0 +1,12 @@
+import kagglehub
+import shutil
+import os
+
+
+def download():
+    destination = "dataset"
+    os.makedirs(destination, exist_ok=True)
+    path = kagglehub.dataset_download("natezhang123/social-anxiety-dataset", force_download=True)
+
+    for file_name in os.listdir(path):
+        shutil.move(os.path.join(path, file_name), os.path.join(destination, file_name))
